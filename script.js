8b68cde8776e170e1a48deea6d1b711194ff998c
Marathena <ang.gomezzp02@gmail.com>
2024-09-26 20:29:18 -0400

    Electric Bogaloo

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let fuera = [g
-]
+let tarot = ["10 of Pentacles: Dale a tu cuerpo alegria, Macarena", 
+    "Nine of Swords: Help, Im actually trapped in the matrix", "The Hanged Man: (Calmly)There is a shark behind you", 
+    "The Fool: Do you know the muffin man?", "5 of Pentacles: If 2+2 is 5, can you pay for my legos?",
+    "Three of Pentacles: All we need is money(and love, ofc)", "The Hierophant: It's dangerous to go alone... Take this",
+    "Ace of Wands: Road work ahead? I sure hope it does."];
+
